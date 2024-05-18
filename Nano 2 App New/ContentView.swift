ae6b56e46172ea185090da8b7f8e3a53fd5d0835
Michelle C <micheelechau@gmail.com>
2024-05-18 22:02:35 +0700

    Initial Commit

---
+++ Nano 2 App New/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nano 2 App New
+//
+//  Created by Michelle Chau on 18/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nano 2 App New/Nano_2_App_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_2_App_NewApp.swift
+//  Nano 2 App New
+//
+//  Created by Michelle Chau on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Nano_2_App_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
