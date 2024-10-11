ab7abcaeef366fd7b0c5ab95e50aa91a079da29d
Michelle C <micheelechau@gmail.com>
2024-10-11 10:03:10 +0700

    test aja

---
+++ Nano 2 App New/Helper/TestGitSoftDelete.swift
@@ -0,0 +1,16 @@
+//
+//  TestGitSoftDelete.swift
+//  Nano 2 App New
+//
+//  Created by Michelle Chau on 11/10/24.
+//
+
+import SwiftUI
+
+
+struct TestGitSoftDeleteTests: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+    }
+}
